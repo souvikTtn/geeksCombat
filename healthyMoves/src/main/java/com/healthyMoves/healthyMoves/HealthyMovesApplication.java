934ca1be60302eee41f23609b8644d406b50e359
Souvik Chakraborty <souvik.chakraborty@tothenew.com>
2021-02-09 11:46:56 +0530

    first commit healthy moves

---
+++ healthyMoves/src/main/java/com/healthyMoves/healthyMoves/HealthyMovesApplication.java
@@ -0,0 +1,13 @@
+package com.healthyMoves.healthyMoves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyMovesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyMovesApplication.class, args);
+	}
+
+}
---
+++ healthyMoves/src/test/java/com/healthyMoves/healthyMoves/HealthyMovesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthyMoves.healthyMoves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyMovesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
